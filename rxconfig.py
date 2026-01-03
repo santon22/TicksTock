791db5b0e3e2816a88da794ff93fca298ba7aed7
santon22 <santon2287@gmail.com>
2026-01-03 10:44:22 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,12 +1,6 @@
 import reflex as rx
-import os
-
-railway_domain = "RAILWAY_PUBLIC_DOMAIN"
 
 config = rx.Config(
-    app_name="your_app_name",
+    app_name="tickstock",  # Replace with your app's module name, e.g., the folder containing your pages/index.py
     telemetry_enabled=False,
-    frontend_port=3000,
-    backend_port=8000,
-    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
