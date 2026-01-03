e4bceefd4ed6d3ca63f308fcc91bc178379bfc8b
santon22 <santon2287@gmail.com>
2026-01-03 10:46:18 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="tickstock",  # Replace with your app's module name, e.g., the folder containing your pages/index.py
+    app_name="TicksTock",  # Replace with your app's module name, e.g., the folder containing your pages/index.py
     telemetry_enabled=False,
 )
