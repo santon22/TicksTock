eda654e4dfdcfb9b0de0ca9ceea742e916482aa2
santon22 <santon2287@gmail.com>
2026-01-03 18:50:45 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,12 @@
 import reflex as rx
+import os
 
-config = rx.Config(app_name="app", plugins=[rx.plugins.TailwindV3Plugin()])
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
+
+config = rx.Config(
+    app_name="Ticks Tock",
+    telemetry_enabled=False,
+    frontend_port=3000,
+    backend_port=8000,
+    api_url=f'https://{os.environ.get(railway_domain)}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
+)
