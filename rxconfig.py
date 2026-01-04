588492f26d7ccd9b951d971ab5f686f6a434e6a7
santon22 <santon2287@gmail.com>
2026-01-03 22:45:02 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Ticks_Tock",  # Must match the folder/filename, e.g., "your_app"
+    app_name="app",  # Must match the folder/filename, e.g., "your_app"
     api_url="https://{your-project}.up.railway.app/api",  # Or use environment var for flexibility
 )
