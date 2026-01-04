dd6341ede963ac8e1891d5d650559ebec817931d
santon22 <santon2287@gmail.com>
2026-01-03 21:45:35 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,8 @@
 import reflex as rx
+import os
 
 config = rx.Config(
-    app_name="Ticks_Tock",
-    api_url=f"https://{{{{RAILWAY_STATIC_URL}}}}/api",
+    app_name="Ticks_Tock",,  # e.g., folder name of your app.py
+    api_url=f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN', '')}" if os.environ.get('RAILWAY_PUBLIC_DOMAIN') else "http://localhost:8000",
+    telemetry_enabled=False,
 )
