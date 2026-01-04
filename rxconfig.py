6b6b5c30f10d2c78e43a2b0a920c5ff8843b1e3b
santon22 <santon2287@gmail.com>
2026-01-03 22:34:42 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,6 @@
 import reflex as rx
 
-config = rx.Config(app_name="app", plugins=[rx.plugins.TailwindV3Plugin()])
+config = rx.Config(
+    app_name="Ticks_Tock",  # Must match the folder/filename, e.g., "your_app"
+    api_url="https://{your-project}.up.railway.app/api",  # Or use environment var for flexibility
+)
