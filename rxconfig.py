621486a9728d8b8e3d1a1192a607c063c921a640
santon22 <santon2287@gmail.com>
2026-01-05 14:14:36 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -5,6 +5,7 @@ railway_domain_env = "RAILWAY_PUBLIC_DOMAIN"
 
 config = rx.Config(
     app_name="app",
+    show_built_with_reflex=False,
     frontend_port=3000, # default frontend port
     backend_port=8000, # default backend port
     api_url=f'https://{os.environ[railway_domain_env]}' if railway_domain_env in os.environ else "http://127.0.0.1:8000",
