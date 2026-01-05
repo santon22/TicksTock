f27c90833de5359433edafef41f31aca0ce49ac5
santon22 <santon2287@gmail.com>
2026-01-05 12:59:45 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,15 @@
 import reflex as rx
+import os
 
-config = rx.Config(app_name="app", plugins=[rx.plugins.TailwindV3Plugin()])
+railway_domain_env = "RAILWAY_PUBLIC_DOMAIN"
+
+config = rx.Config(
+    app_name="reflex_template",
+    frontend_port=3000, # default frontend port
+    backend_port=8000, # default backend port
+    api_url=f'https://{os.environ[railway_domain_env]}' if railway_domain_env in os.environ else "http://127.0.0.1:8000",
+    plugins=[
+        rx.plugins.SitemapPlugin(),
+        rx.plugins.TailwindV4Plugin(),
+    ]
+)
