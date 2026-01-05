0ef2fc10b848a52fbfb92dad26e1fadc120b2426
santon22 <santon2287@gmail.com>
2026-01-05 13:22:13 -0600

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import os
 railway_domain_env = "RAILWAY_PUBLIC_DOMAIN"
 
 config = rx.Config(
-    app_name="reflex_template",
+    app_name="app",
     frontend_port=3000, # default frontend port
     backend_port=8000, # default backend port
     api_url=f'https://{os.environ[railway_domain_env]}' if railway_domain_env in os.environ else "http://127.0.0.1:8000",
